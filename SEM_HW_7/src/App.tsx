6281dcd2bda8f85d42792b999af9235d38ca9b07
nessornot <nessornot404@gmail.com>
2026-01-16 00:04:16 +0300

    feat: add notification component

---
+++ SEM_HW_7/src/App.tsx
@@ -0,0 +1,11 @@
+import NotificationComponent from "./components/NotificationComponent.tsx";
+
+function App() {
+  return (
+    <>
+      <NotificationComponent />
+    </>
+  )
+}
+
+export default App
